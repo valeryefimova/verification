0e08af4d5050b4431b26884b106f6cb5a939eba6
Valeria Efimova <valeryefimova@gmail.com>
2018-06-20 14:57:30 +0300

    toLtlBA convetation added from Formula object using ltl2ba library.

--- src/main/java/com/efimova/verification/Main.java
+++ src/main/java/com/efimova/verification/Main.java
@@ -10,12 +10,14 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         String ltlFormulaString = args[0];
 
         LtlFormulaParser parser = new LtlFormulaParser();
         Formula formula = parser.parse(ltlFormulaString);
         System.out.println(formula.toString());
+        System.out.println(formula.ltlToAutomaton());
+
+        System.out.println();
 
         File diagramXmlFile = new File(args[1]);
         DiagramParser diagramService = new DiagramParser();
--- src/main/java/com/efimova/verification/ltl/Formula.java
+++ src/main/java/com/efimova/verification/ltl/Formula.java
@@ -1,7 +1,14 @@
 package com.efimova.verification.ltl;
 
 import com.efimova.verification.LtlVisitor;
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 
@@ -9,4 +16,50 @@ public abstract class Formula {
     public abstract Formula transformName(Function<String, String> transformer);
 
     public abstract void accept(LtlVisitor visitor);
+
+
+    public String ltlToAutomaton() {
+        Map<String, String> oldToNeNamesMap = new HashMap<>();
+
+        // replace all names with vX, where x is a number:
+        Formula lowercaseLTL = this.transformName(name ->
+                oldToNeNamesMap.computeIfAbsent(name, v -> "v" + oldToNeNamesMap.size()));
+
+        // replace Release to V in orders to comply spin syntax:
+        String spinSyntaxFormula = lowercaseLTL.toString().replace("R", "V");
+        return spinLtlToBA(spinSyntaxFormula);
+    }
+
+
+    @SneakyThrows
+    private String spinLtlToBA(String ltl) {
+        String cygwinBash = "C:\\lib\\cygwin64\\bin\\bash.exe";
+        String ltl2baExecutable = "ltl2ba/ltl2ba.exe";
+        String configFile = "src/main/resources/config.properties";
+
+        BufferedReader reader = new BufferedReader(new FileReader(configFile));
+        String s = reader.readLine();
+        if (!s.isEmpty()) {
+            cygwinBash = s;
+            s = reader.readLine();
+            ltl2baExecutable = s;
+        } else {
+            System.err.println("Unable to read config file: " + configFile);
+        }
+
+        String command = ltl2baExecutable + " -f \\\"" + ltl + "\\\"";  // windows way
+
+        Process process = Runtime.getRuntime().exec(new String[]{cygwinBash, "-c", command});
+        process.waitFor();
+
+        StringWriter errorWriter = new StringWriter();
+        IOUtils.copy(process.getErrorStream(), errorWriter);
+        if (errorWriter.toString().length() > 0) {
+            System.err.println(errorWriter.toString());
+        }
+
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(process.getInputStream(), writer);
+        return writer.toString();
+    }
 }
